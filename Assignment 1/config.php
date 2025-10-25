06451cff2e7a08cb6d716af86858d5ba0f6b9e3a
group-6eventmanagement <Laeltonymn@gmail.com>
2025-10-25 16:50:15 +0100

    Delete Assignment 1 directory

--- Assignment 1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Copy this file to config.php and set your database credentials.
-// Example:
-// $servername = 'localhost';
-// $username = 'root';
-// $password = '';
-// $dbname = 'event_registration_system';
-?>
